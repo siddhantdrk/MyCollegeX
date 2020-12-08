1d5a2820fe2b4b0e42aa3395d9f1c3651d0b16a1
Rahul Dev <iit2019053@iiita.ac.in>
2020-12-08 22:22:45 +0530

    apply intent

--- app/src/main/java/com/example/mycollegex/LoginActivity.java
+++ app/src/main/java/com/example/mycollegex/LoginActivity.java
@@ -1,14 +1,44 @@
 package com.example.mycollegex;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView mSignUpTextView;
+    private ImageView mLogoImage;
+    private MaterialButton mGoogleLoginButton,mSignInButton;
+    private TextInputEditText mEmailLogin,mPasswordLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initViews();
+
+        mSignUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUpIntent = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(signUpIntent);
+            }
+        });
+
+    }
+
+    private void initViews() {
+        mSignUpTextView = (TextView)findViewById(R.id.signUp_txt);
+        mLogoImage = (ImageView)findViewById(R.id.logo_image_view);
+        mGoogleLoginButton = (MaterialButton)findViewById(R.id.google_login_btn);
+        mSignInButton = (MaterialButton)findViewById(R.id.sign_in_btn);
+        mEmailLogin = (TextInputEditText)findViewById(R.id.email_edit_text_login);
+        mPasswordLogin = (TextInputEditText)findViewById(R.id.pass_edit_text_login);
     }
 }
--- app/src/main/java/com/example/mycollegex/WelcomeActivity.java
+++ app/src/main/java/com/example/mycollegex/WelcomeActivity.java
@@ -1,14 +1,53 @@
 package com.example.mycollegex;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class WelcomeActivity extends AppCompatActivity {
+    private MaterialButton mRegisterButton,mLoginButton;
+    private TextView mAboutApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initViews();
+
+//        mAboutApp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent signUpIntent = new Intent(WelcomeActivity.this,AboutAppActivity.class);
+//                startActivity(signUpIntent);
+//            }
+//        });
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUpIntent = new Intent(WelcomeActivity.this,SignUpActivity.class);
+                startActivity(signUpIntent);
+            }
+        });
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUpIntent = new Intent(WelcomeActivity.this,LoginActivity.class);
+                startActivity(signUpIntent);
+            }
+        });
+
+    }
+
+    private void initViews() {
+        mAboutApp=(TextView)findViewById(R.id.about_app_textView);
+        mRegisterButton =(MaterialButton)findViewById(R.id.login_register_btn);
+        mLoginButton = (MaterialButton)findViewById(R.id.login_btn);
     }
 }
