c4d9826a1f6a4d175d95d34e2f5eb0e9b458c7d6
Mandar16 <iit2019059@iiita.ac.in>
2020-12-10 14:39:33 +0530

    new Intent

--- app/src/main/java/com/example/mycollegex/Adapters/PostAdapter.java
+++ app/src/main/java/com/example/mycollegex/Adapters/PostAdapter.java
@@ -1,5 +1,6 @@
 package com.example.mycollegex.Adapters;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mycollegex.ContactSellerActivity;
 import com.example.mycollegex.MainActivity;
 import com.example.mycollegex.R;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
@@ -42,6 +44,15 @@ public class PostAdapter extends FirestoreRecyclerAdapter<PostsItems,PostAdapter
 
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
        // String currentUserId = currentUser.getUid();
+        holder.likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),ContactSellerActivity.class);
+                view.getContext().startActivity(intent);
+
+
+            }
+        });
 
 
 
