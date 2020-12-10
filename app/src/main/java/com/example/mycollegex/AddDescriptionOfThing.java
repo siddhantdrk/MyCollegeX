f97c4258ab382265adbaefc93a5cc7522375b062
Mandar16 <iit2019059@iiita.ac.in>
2020-12-10 13:45:56 +0530

    added intent from post details to MainActivity

--- app/src/main/java/com/example/mycollegex/AddDescriptionOfThing.java
+++ app/src/main/java/com/example/mycollegex/AddDescriptionOfThing.java
@@ -2,6 +2,7 @@ package com.example.mycollegex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,6 +32,9 @@ public class AddDescriptionOfThing extends AppCompatActivity {
                     postDaos.addPost(toPost);
 
                 }
+
+                Intent intent =new Intent(AddDescriptionOfThing.this,MainActivity.class);
+                startActivity(intent);
             }
         });
     }
